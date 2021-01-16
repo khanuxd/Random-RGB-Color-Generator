84f26b249a18d343187a9946301527b308342fbe
Khan Md Fayjul Kabir <43878538+KhanUXD@users.noreply.github.com>
2021-01-17 01:35:46 +0200

    init

--- ranColor.js
+++ ranColor.js
@@ -1,13 +1,16 @@
 const btn = document.querySelector('button');
-
 const h1 = document.querySelector('h1');
+
+// adding click event button
 
 btn.addEventListener('click', function(){
     const newColor = makeRandColor();
     document.body.style.backgroundColor = newColor;
-    h1.style.color = 'rgb(255, 255, 255)'
+    h1.style.color = newColor;
     h1.innerText = newColor;
 })
+
+// creating a callback function
 
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 256);
